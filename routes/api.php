9a3afcda038eb05fd1207d81251e7cbc2de72d94
Sherange Fonseka <sherangef@gmail.com>
2018-02-25 09:46:16 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('user-login','UserController@userLogin');
+Route::post('user-registration', 'UserController@userRegistration');
+Route::group(['middleware' => 'auth:api'], function(){
+    //auth user routes
+    Route::get('userdetails','UserController@userDetails');
+
+});
